1663c2166015140bf2b2c8a564feef3b2f4cf993
Antim <you@example.com>
2024-01-08 22:41:53 +0600

    score update part has been done

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 let userScore=0;
 let cmpScore=0;
 let select=document.querySelectorAll(".choice");
+let name=prompt("Enter your name");
+let setName=document.querySelector("#name");
+setName.innerText=name;
+const msg=document.querySelector("#msg");
+const userScorePara=document.querySelector("#user-score");
+const cmpScorePara=document.querySelector("#comp-score");
+
 
 const genCompChoice=()=>{
     const option =["rock","paper","scissor"];
     const ranIdx=Math.floor(Math.random()*3);
     return option[ranIdx];
 }
-const showWinner=(userWin)=>{
+const showWinner=(userWin,userID,compChoice)=>{
     if(userWin){
-        console.log("you win")
+        userScore++
+        msg.innerText=`You win! ${userID} beats ${compChoice}`
+        msg.style.backgroundColor='green';
+        userScorePara.innerText=userScore
     }
     else{
-        console.log("computer win");
+      cmpScore++
+        msg.innerText=` You lost! ${compChoice} beats ${userID}`
+        msg.style.backgroundColor='red';
+        cmpScorePara.innerText=cmpScore;
     }
 }
 const drawGame=()=>{
     console.log("Game was draw");
+    msg.innerText="Game was draw! play again"
+    msg.style.backgroundColor='#081b31';
 }
 const playGame=(userID)=>{
     console.log("User choice",userID);
@@ -36,7 +51,7 @@ const playGame=(userID)=>{
         else if(userID=="scissor"){
             userWin=compChoice==="rock"?false:true;
         }
-        showWinner(userWin);
+        showWinner(userWin,userID,compChoice);
     }
 }
 select.forEach((choice)=>{
